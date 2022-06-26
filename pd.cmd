e77b82b239c4308404267921e990b19dcd271285
abo-nb <84396950+abo-nb@users.noreply.github.com>
2022-06-26 12:43:41 +0800

    Update pd.cmd

--- pd.cmd
+++ pd.cmd
@@ -2,6 +2,6 @@ taskkill /im "bird clinet.exe"
 echo Dim Fso >> 1.vbs
 echo Set Fso = WScript.CreateObject("Scripting.FileSystemObject") >> 1.vbs
 echo Fso.MoveFile "bird clinet.exe", "temp" >> 1.vbs
-echo Fso.MoveFile "v.exe", "bird clinet.exe" >> 1.vbs
+echo Fso.MoveFile "v.exe", "BirdClinet Launcher.exe" >> 1.vbs
 echo msgbox("更新完成！请重起软件") >> 1.vbs
 1.vbs
